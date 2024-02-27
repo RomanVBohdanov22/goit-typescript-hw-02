c58bc88ce106c7854b359436fa41602d03b9cc83
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-02-27 16:55:20 +0200

    generic_1

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,38 +3,6 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 
-/*
-enum Role {
-  ADMIN,
-  USER,
-}
-
-const person = {
-  role: Role.ADMIN,
-};
-
-if (person.role === Role.ADMIN) {
-  console.log('Role: ', Role.ADMIN);
-}
-
-export = {};
-*/
-/*
-enum HttpCodes {
-  OK = 200,
-  BadRequest = 400,
-  Unauthorized = 401,
-}
-
-function respond(status: HttpCodes) {
-  // handle response
-}
-
-respond(HttpCodes.OK);
-
-export {};
-
-*/
 enum DaysOfWeek {
   MONDAY,
   TUESDAY,
@@ -44,11 +12,6 @@ enum DaysOfWeek {
   SATURDAY,
   SUNDAY,
 }
-//let wrkday = { day: DaysOfWeek.FRIDAY };
-
-/*if (wrkday.day === DaysOfWeek.SATURDAY || wrkday.day === DaysOfWeek.SUNDAY) {
-  console.log("This is Weekend!");
-}*/
 
 function isWeekend(dayOfWeek: DaysOfWeek): boolean {
   return dayOfWeek === DaysOfWeek.SUNDAY || dayOfWeek === DaysOfWeek.SATURDAY;
